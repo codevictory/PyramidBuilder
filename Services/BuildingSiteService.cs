97bbdedbc9d07bc10fd0fe9cae1d8eadb72598cd
codevictory <main@codevictory.net>
2020-08-31 17:03:50 +0300

    Building site service project.

---
+++ Services/BuildingSiteService.cs
@@ -0,0 +1,12 @@
+namespace Services
+{
+    public class BuildingSiteService : IBuildingSiteService
+    {
+        public int pyramidHeight { get; set; }
+        
+        public int GetPyramidSideLength()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Services/IBuildingSiteService.cs
@@ -0,0 +1,7 @@
+namespace Services
+{
+    public interface IBuildingSiteService
+    {
+        int GetPyramidSideLength();
+    }
+}
