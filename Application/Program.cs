ec3dceb2ed79c097d3e2ee5de75100e8ad5d8766
codevictory <main@codevictory.net>
2020-09-01 22:41:51 +0300

    Initial UI.

--- Application/Program.cs
+++ Application/Program.cs
@@ -7,6 +7,29 @@ namespace Application
         static void Main(string[] args)
         {
             Console.WriteLine("Pharaoh demands a new pyramid!");
+
+            System.Console.WriteLine("What should be the height of the pyramid be?");
+
+            var height = Convert.ToInt32(System.Console.ReadLine());
+
+            var nextLevel = 1;
+            var air = height - 1;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < air; j++)
+                {
+                    System.Console.Write(' ');
+                }
+                for (int j = 0; j < nextLevel; j++)
+                    System.Console.Write('#');
+                System.Console.Write('\n');
+                nextLevel += 2;
+                air--;
+            }
+
+            System.Console.WriteLine("\nPyramid is ready! Press enter to exit.");
+
+            var end = System.Console.ReadLine();
         }
     }
 }
